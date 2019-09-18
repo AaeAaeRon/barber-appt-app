56e8118647da32ac98641e0c32a1a7d389c475e5
Aaron Briscoe <abriscoe2008gmail.com>
2019-09-18 17:53:57 -0500

    d

--- server/app/controllers/auth_controller.rb
+++ server/app/controllers/auth_controller.rb
@@ -2,19 +2,19 @@ class AuthController < ApplicationController
     # skip_before_action :check_authentication, only: [:create]
 
     def loginClient
-        byebug
-            client = Client.find_by(email: params[:email])
+        # byebug
+        client = Client.find_by(email: params[:email])
 
-            if client && client.authenticate(params[:password])
-                render json: {email: client.email, id: client.id, token: encode_token({client_id: client.id}) }
-            else 
-                render json: {error: 'Invalid email or password'}
-            end 
+        if client && client.authenticate(params[:password])
+            render json: {email: client.email, id: client.id, token: encode_token({client_id: client.id}) }
+        else 
+            render json: {error: 'Invalid email or password'}
+        end 
         
     end
 
     def loginProf
-        byebug
+        # byebug
         professional = Professional.find_by(email: params[:email])
 
         if professional && professional.authenticate(params[:password])
